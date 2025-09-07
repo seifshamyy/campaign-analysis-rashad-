c1d39fff465150e6abfecc79e192e51f2b5f16ff
seifshamyy <drseifshamy@gmail.com>
2025-09-08 01:16:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const path = require('path');
 const app = express();
 app.disable('x-powered-by');
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'public'), {
+
+// serve all static assets from the repo root
+app.use(express.static(__dirname, {
   setHeaders(res, filePath) {
-    if (/\.(html)$/.test(filePath)) {
+    if (/\.html?$/.test(filePath)) {
       res.setHeader('Cache-Control', 'no-store');
     } else {
       res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
@@ -15,8 +17,12 @@ app.use(express.static(path.join(__dirname, 'public'), {
   }
 }));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// healthcheck (optional)
+app.get('/healthz', (_req, res) => res.status(200).send('ok'));
+
+// send index.html for all other routes
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
